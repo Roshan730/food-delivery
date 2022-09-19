b18135bfd45f32ef3897a030f468dacc860308b1
lucifer098a <nk5141030@gmail.com>
2022-09-20 02:39:39 +0530

    mainContainer

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import B1 from "../img/b1.png";
+import M1 from "../img/m1.png";
+
+export const heroData = [
+  {
+    id: 1,
+    name: "IceCream",
+    decp: "Chocolate & Vanilla",
+    price: "90",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Fresh Strawberries",
+    decp: "Fresh Strawberries",
+    price: "200",
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "Burger",
+    decp: "Veg Burger",
+    price: "130",
+    imageSrc: B1,
+  },
+  {
+    id: 4,
+    name: "Streamed Momos",
+    decp: "Veg Momos",
+    price: "170",
+    imageSrc: M1,
+  },
+];
